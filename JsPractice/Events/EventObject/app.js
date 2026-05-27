9ad11ea08f0f8da4a06464a520f2f63b80f9abbd
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-27 20:55:10 +0530

    create eventobject

---
+++ JsPractice/Events/EventObject/app.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("input")
+
+// input.addEventListener('keydown', function (e) {
+//     console.log(e.key)
+//     console.log(e.code)
+// })
+
+window.addEventListener('keydown', function (e) {
+    switch (e.code) {
+        case "ArrowUp":
+            console.log("UP")
+            break
+        case "ArrowDown":
+            console.log("DOWN")
+            break
+        default:
+            console.log("Ignore")
+            break
+    }
+})
