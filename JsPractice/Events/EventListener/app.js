50ab376e66ecfeb16fb5209890c73cf16f380e49
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-27 14:17:45 +0530

    Update app.js

--- JsPractice/Events/EventListener/app.js
+++ JsPractice/Events/EventListener/app.js
@@ -1,4 +1,15 @@
 const btn = document.querySelector('button')
-btn.addEventListener('click', () => {
-    console.log("Eventlistener")
-})
+// btn.addEventListener('click', () => {
+//     console.log("Eventlistener")
+// })
+
+function one() {
+    console.log("one")
+}
+
+function two() {
+    console.log("two")
+}
+
+btn.addEventListener('click', one, { once: true })
+btn.addEventListener('click', two)
