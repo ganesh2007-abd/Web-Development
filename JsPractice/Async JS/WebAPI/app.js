77d0cf1f75862f87ccadf984c2144a61632d0e70
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-05-29 22:02:03 +0530

    webAPI
    Js handles some functions to webapi to take care of

---
+++ JsPractice/Async JS/WebAPI/app.js
@@ -0,0 +1,21 @@
+setTimeout(function () {
+    document.body.style.backgroundColor = 'violet'
+    setTimeout(function () {
+        document.body.style.backgroundColor = 'indigo'
+        setTimeout(function () {
+            document.body.style.backgroundColor = 'blue'
+            setTimeout(function () {
+                document.body.style.backgroundColor = 'green'
+                setTimeout(function () {
+                    document.body.style.backgroundColor = 'yellow'
+                    setTimeout(function () {
+                        document.body.style.backgroundColor = 'orange'
+                        setTimeout(function () {
+                            document.body.style.backgroundColor = 'red'
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
