0d90a49d878c786efeb4cd3eed9933b2946cf131
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-09 20:29:05 +0530

    delayedcolorchange using promises

---
+++ JsPractice/Async JS/Promises_2/app.js
@@ -0,0 +1,37 @@
+const colordelay = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color
+            resolve(`${color}`)
+        }, delay)
+    })
+}
+
+colordelay('red', 1000)
+    .then((data) => {
+        console.log(data)
+        return colordelay('orange', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+        return colordelay('yellow', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+        return colordelay('green', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+        return colordelay('blue', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+        return colordelay('indigo', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+        return colordelay('violet', 1000)
+    })
+    .then((data) => {
+        console.log(data)
+    })
