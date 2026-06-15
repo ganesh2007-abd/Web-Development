ed744efce7523254283b54b0b0e7adebba5eb253
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-15 12:04:27 +0530

    using req,res objects
    These are parsed objects automatically by JS

--- JsPractice/ExpressPractice/BasicExpressApp/index.js
+++ JsPractice/ExpressPractice/BasicExpressApp/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.use(() => {
+app.use((req, res) => {
     console.log("Got a request!!")
+    // res.send('<button>This is a response</button>')
+    // res.send({ name: 'ganesh', age: 19 })
+    res.send("Ganesh")
 })
 
 app.listen(3000, () => {
