5278be3dae70949b8e3d35c86a26a8927487a81c
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-15 12:32:04 +0530

    adding routes using app.get() and app.post()

--- JsPractice/ExpressPractice/BasicExpressApp/index.js
+++ JsPractice/ExpressPractice/BasicExpressApp/index.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-    console.log("Got a request!!")
-    // res.send('<button>This is a response</button>')
-    // res.send({ name: 'ganesh', age: 19 })
-    res.send("Ganesh")
+// app.use((req, res) => {
+//     console.log("Got a request!!")
+//     // res.send('<button>This is a response</button>')
+//     // res.send({ name: 'ganesh', age: 19 })
+//     // res.send("Ganesh")
+// })
+
+app.get('/cats', (req, res) => {
+    res.send("MEOW")
+})
+
+app.get('/dogs', (req, res) => {
+    res.send("WOOF")
+})
+
+app.get('/', (req, res) => {
+    res.send("Welcome Home!")
+})
+
+app.post('/cats', (req, res) => {
+    res.send("MEOW(POST)")
+})
+
+app.post('/dogs', (req, res) => {
+    res.send("WOOF(POST)")
+})
+
+app.post('/', (req, res) => {
+    res.send("WELCOME HOME!(POST)")
+})
+
+app.get('/{*path}', (req, res, next) => {
+    res.send("No route found!")
 })
 
 app.listen(3000, () => {
