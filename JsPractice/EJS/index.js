d1212f966f604be966b0db5b5e398157d8cebf71
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-16 21:55:18 +0530

    tried real json data (subreddit demo)

--- JsPractice/EJS/index.js
+++ JsPractice/EJS/index.js
@@ -1,29 +1,33 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const Rdata = require('./data.json')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/', (req, res) => {
-    const rand = Math.floor(Math.random() * 10) + 1
-    // res.send('Hello')
-    res.render('home.ejs', { rand: rand })
-})
+// app.get('/', (req, res) => {
+//     const rand = Math.floor(Math.random() * 10) + 1
+//     // res.send('Hello')
+//     res.render('home.ejs', { rand: rand })
+// })
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params
-    res.render('home.ejs', { subreddit: subreddit })
+    const data = Rdata[subreddit]
+    // console.log(data)
+    res.render('subreddit.ejs', { data })
+    // res.render('home.ejs', { subreddit: subreddit })
 })
 
 // app.get('/conditionals', (req, res) => {
 //     res.render('conditionals.ejs')
 // })
 
-app.get('/cats', (req, res) => {
-    const cats = ['ganesh', 'honey', 'suvarna', 'rambabu']
-    res.render('loops', { cats })
-})
+// app.get('/cats', (req, res) => {
+//     const cats = ['ganesh', 'honey', 'suvarna', 'rambabu']
+//     res.render('loops', { cats })
+// })
 
 app.listen(3000, () => {
     console.log("Listening on 3000")
