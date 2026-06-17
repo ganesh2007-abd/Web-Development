e8b6e9e354f333f3e97563203b2e72acc86f48da
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-17 17:42:13 +0530

    created indexroute

--- JsPractice/RestRoutes/GetVsPost/index.js
+++ JsPractice/RestRoutes/GetVsPost/index.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+const comments = [
+    {
+        user: 'ganesh',
+        text: 'I have a group of companies'
+    },
+    {
+        user: 'lakshmi',
+        text: 'i have a son having group of companies,lol'
+    },
+    {
+        user: 'suvarna',
+        text: 'I have a brother having group of companies'
+    },
+    {
+        user: 'rambabu',
+        text: 'If my boy has comapany i too have a company'
+    }
+]
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
 
 app.get('/home', (req, res) => {
     res.send("This is get response")
