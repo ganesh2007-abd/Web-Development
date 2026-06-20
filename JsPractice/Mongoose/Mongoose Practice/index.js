21bd6e16d2619f11afa692e89d8b094c653f9940
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-20 13:35:44 +0530

    Defing a schema and model

--- JsPractice/Mongoose/Mongoose Practice/index.js
+++ JsPractice/Mongoose/Mongoose Practice/index.js
@@ -8,4 +8,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
         console.log(err)
     })
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    review: Number,
+    rating: String
+})
 
+const Movie = mongoose.model('Movie', movieSchema)
+const amadeus = new Movie({ title: "Amadeus", year: 1996, review: 9.2, rating: 'R' })
