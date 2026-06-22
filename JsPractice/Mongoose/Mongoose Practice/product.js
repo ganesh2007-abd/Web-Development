9bc9f85c852576a8ed42bc7c90550011cc01c11e
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-22 17:02:00 +0530

    just changes the db name

--- JsPractice/Mongoose/Mongoose Practice/product.js
+++ JsPractice/Mongoose/Mongoose Practice/product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
     .then(() => {
         console.log("Connection Open!")
     })
