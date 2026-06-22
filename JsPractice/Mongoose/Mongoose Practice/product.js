8f48e05207bce2e4c87aa7d51df99016f8922673
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-22 18:12:13 +0530

    added custom methods for our schema

--- JsPractice/Mongoose/Mongoose Practice/product.js
+++ JsPractice/Mongoose/Mongoose Practice/product.js
@@ -40,17 +40,40 @@ const product = new mongoose.Schema({
     }
 })
 
+product.methods.greet = function () {
+    console.log(`Hello from ${this.name}`)
+}
+
+product.methods.toggleonsale = function () {
+    this.onSale = !this.onSale
+    return this.save()
+}
+
+product.methods.addcategory = function (newcat) {
+    this.categories.push(newcat)
+    return this.save()
+}
+
 const Product = mongoose.model('Product', product)
-const jersey = new Product({ name: 'RCB Jersey', price: -1400, categories: ['RCB', 'cricket', 'virat', 'bengaluru'], size: 'M' })
-jersey.save()
-    .then(data => {
-        console.log("It worked")
-        console.log(data)
-    })
-    .catch(err => {
-        console.log("Error")
-        console.log(err)
-    })
+
+const findproduct = async () => {
+    const foundprod = await Product.findOne({ name: 'Tire Pump' })
+    foundprod.greet()
+    await foundprod.toggleonsale()
+    await foundprod.addcategory('air filling')
+}
+
+
+// const jersey = new Product({ name: 'RCB Jersey', price: -1400, categories: ['RCB', 'cricket', 'virat', 'bengaluru'], size: 'M' })
+// jersey.save()
+//     .then(data => {
+//         console.log("It worked")
+//         console.log(data)
+//     })
+//     .catch(err => {
+//         console.log("Error")
+//         console.log(err)
+//     })
 
 
 // Product.findOneAndUpdate({ name: "Mountain Bike" }, { name: "Speeding car", price: -300000, categories: ['Racing', 'Car'] }, { new: true, runValidators: true })
