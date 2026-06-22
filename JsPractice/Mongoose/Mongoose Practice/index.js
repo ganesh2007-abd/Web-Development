04457daecd62b05ecdf7fb822a657ab9b8f6a75c
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-22 13:09:55 +0530

    insertMany() in code

--- JsPractice/Mongoose/Mongoose Practice/index.js
+++ JsPractice/Mongoose/Mongoose Practice/index.js
@@ -17,3 +17,14 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema)
 const amadeus = new Movie({ title: "Amadeus", year: 1996, review: 9.2, rating: 'R' })
+
+Movie.insertMany([
+    { title: 'Bahubali', year: 2014, review: 9.6, rating: "A+" },
+    { title: 'Bahubali 2', year: 2018, review: 9.7, rating: "A+" },
+    { title: 'Animal', year: 2023, review: 8.6, rating: "A" },
+    { title: 'Kantara', year: 2026, review: 9.6, rating: "A+" },
+])
+    .then(data => {
+        console.log("Inserted")
+        console.log(data)
+    })
