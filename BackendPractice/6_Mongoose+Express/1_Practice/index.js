592551629904b6b3acacc8e384fa0a52bd269ba9
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-23 16:24:14 +0530

    added Update route

--- BackendPractice/6_Mongoose+Express/1_Practice/index.js
+++ BackendPractice/6_Mongoose+Express/1_Practice/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const ejs = require('ejs')
+const methodOverride = require('method-override')
 
 const mongoose = require('mongoose')
 
@@ -22,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', ejs)
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const prods = await Product.find({})
@@ -36,19 +38,29 @@ app.get('/products/new', (req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
-    console.log(product)
+    // console.log(product)
     res.render('products/show.ejs', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit.ejs', { product })
 })
 
 app.post('/products', async (req, res) => {
     const newprod = new Product(req.body)
-    console.log(newprod)
+    // console.log(newprod)
     await newprod.save(
         res.redirect('/products/')
     )
 })
 
-
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const foundproduct = await Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+    res.redirect(`/products/${foundproduct._id}`)
+})
 
 app.listen(3000, () => {
     console.log("Listening on 3000 port")
