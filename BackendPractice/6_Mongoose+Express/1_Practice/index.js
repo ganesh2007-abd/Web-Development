702dbbc9a21b81c149baeb5082d3ea3d0a051ffa
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-23 15:50:16 +0530

    added create route

--- BackendPractice/6_Mongoose+Express/1_Practice/index.js
+++ BackendPractice/6_Mongoose+Express/1_Practice/index.js
@@ -17,12 +17,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/FarmStand')
         console.log(err)
     })
 
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', ejs)
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
     const prods = await Product.find({})
     res.render('products/index.ejs', { prods })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new.ejs')
+    // res.send("New product")
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -31,6 +39,16 @@ app.get('/products/:id', async (req, res) => {
     console.log(product)
     res.render('products/show.ejs', { product })
 })
+
+app.post('/products', async (req, res) => {
+    const newprod = new Product(req.body)
+    console.log(newprod)
+    await newprod.save(
+        res.redirect('/products/')
+    )
+})
+
+
 
 app.listen(3000, () => {
     console.log("Listening on 3000 port")
