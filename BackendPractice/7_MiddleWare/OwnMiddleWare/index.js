4930582a77acf98584cd28e829808451013c2d82
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-25 14:10:45 +0530

    More middleware practice

--- BackendPractice/7_MiddleWare/OwnMiddleWare/index.js
+++ BackendPractice/7_MiddleWare/OwnMiddleWare/index.js
@@ -5,25 +5,30 @@ const app = express()
 
 app.use(morgan('tiny'))
 
+// app.use((req, res, next) => {
+//     console.log("First Middleware")
+//     next()
+//     console.log("First Middleware 2")
+// })
+// app.use((req, res, next) => {
+//     console.log("second Middleware")
+//     next()
+//     console.log("second Middleware 2")
+// })
+// app.use((req, res, next) => {
+//     console.log("third Middleware")
+//     return next()
+//     console.log("third Middleware - 2")
+// })
+// app.use((req, res, next) => {
+//     console.log("fourth Middleware")
+//     next()
+//     console.log("fourth Middleware - 2")
+// })
+
 app.use((req, res, next) => {
-    console.log("First Middleware")
+    console.log(req.method.toUpperCase(), req.path)
     next()
-    console.log("First Middleware 2")
-})
-app.use((req, res, next) => {
-    console.log("second Middleware")
-    next()
-    console.log("second Middleware 2")
-})
-app.use((req, res, next) => {
-    console.log("third Middleware")
-    return next()
-    console.log("third Middleware - 2")
-})
-app.use((req, res, next) => {
-    console.log("fourth Middleware")
-    next()
-    console.log("fourth Middleware - 2")
 })
 
 app.get('/', (req, res) => {
