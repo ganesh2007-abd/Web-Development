576efee9603546e0f784b0a9bdbf5c742794f964
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-30 12:25:22 +0530

    one to few relationship in MongoDB

---
+++ BackendPractice/8_MongoRelationships/Demo/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/MRDemo')
+    .then(() => {
+        console.log("Mongodb Connection Open!")
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error")
+        console.log(err)
+    })
+
+const userschema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            _id: { _id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userschema)
+
+const makeuser = async () => {
+    const u = new User({
+        first: 'ganesh',
+        last: 'bonda',
+    })
+    u.addresses.push({
+        street: 'chaganti vari street',
+        city: 'Tanuku',
+        state: 'Ap',
+        country: 'India'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+const addaddress = async (id) => {
+    const user = await User.findById(id)
+    user.addresses.push({
+        street: 'sasi colony',
+        city: 'velivennu',
+        state: 'ap',
+        country: 'India'
+    })
+    const res = await user.save()
+    console.log(res)
+}
+
+makeuser()
+addaddress('6a4366f4fecbf361bc4e457e')
