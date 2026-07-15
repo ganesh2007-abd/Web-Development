ab50da4e2727607b11367777812f7a2891dfe2c7
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-07-15 14:19:16 +0530

    created register route

--- BackendPractice/12_Authentication/1_AuthDemo/index.js
+++ BackendPractice/12_Authentication/1_AuthDemo/index.js
@@ -5,13 +5,34 @@ const app = express()
 const bcrypt = require('bcrypt')
 const User = require('./models/user')
 
+mongoose.connect('mongodb://127.0.0.1:27017/AuthDemo')
+    .then(() => {
+        console.log("Mongodb Connection Open!")
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error")
+        console.log(err)
+    })
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body
+    const hash = await bcrypt.hash(password, 12)
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save()
+    res.redirect('/secret')
+})
 
 app.get('/secret', (req, res) => {
     res.send('this is a secret!!')
