487ef5da818e8c485b7ceddbac2e79ce5817feea
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-08-03 09:57:48 +0530

    tweaking with react css

--- ReactPractice/vite+react_tuts_!/src/conditionals.jsx
+++ ReactPractice/vite+react_tuts_!/src/conditionals.jsx
@@ -1,13 +1,14 @@
 export default function Conditionals() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
+    const styles = { color: num1 === num2 ? "green" : "red" }
     return (
-        <>
+        <div style={styles}>
 
             {num1 === num2 ? <h1>You Win</h1> : <h1>You lose:(</h1>}
             <p>{num1}</p>
             <p>{num2}</p>
 
-        </>
+        </div>
     )
 }
