f23a3af42f7888afacdf4528686ec6ed03432829
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-08-06 14:57:29 +0530

    trued the onMouseOver event

--- ReactPractice/vite+react_tuts_!/src/Clicker.jsx
+++ ReactPractice/vite+react_tuts_!/src/Clicker.jsx
@@ -2,10 +2,15 @@ function handleClick() {
     console.log("Clicked the Button!")
 }
 
+
+function handleHover() {
+    console.log("Hovered over it!")
+}
 export default function Clicker() {
 
     return (
         <div>
+            <p onMouseOver={handleHover}>Hover over me</p>
             <button onClick={handleClick}>Click</button>
         </div>
     )
