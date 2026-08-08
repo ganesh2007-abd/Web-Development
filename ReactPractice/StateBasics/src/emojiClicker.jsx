d9e146673da382df3f4012d8041f9eb06467a8f6
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-08-08 11:40:47 +0530

    array of objects -- implementation of uuid

--- ReactPractice/StateBasics/src/emojiClicker.jsx
+++ ReactPractice/StateBasics/src/emojiClicker.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function EmojiClicker() {
-    const [emoji, setEmoji] = useState([":)"])
+    const [emoji, setEmoji] = useState([{ id: uuid(), emoji: ":)" }])
     const addEmoji = () => {
-        setEmoji((oldEmojis) => [...oldEmojis, ":)"])
+        setEmoji((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: ":)" }])
     }
     return (
         <div>
             {emoji.map((e) => (
-                <span style={{ fontSize: "4rem" }}>{e}</span>
+                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
