9ec05db1ca00ed68f7ad9fe0f434f99f51104f5f
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-08-26 15:51:19 +0530

    useEffect dependencies as 2nd arg to it

--- ReactPractice/useEffectHook/src/couter.jsx
+++ ReactPractice/useEffectHook/src/couter.jsx
@@ -2,17 +2,25 @@ import { useState, useEffect } from "react";
 
 export default function Counter() {
     const [count, setcount] = useState(0);
+    const [name, setname] = useState("")
     useEffect(function myEffect() {
         console.log("my effect is called!!")
-    })
+    }, [count])
+
     const increment = () => {
         setcount((c) => c + 1)
+    }
+
+    const handleChange = (e) => {
+        setname(e.target.value)
     }
 
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>+1</button>
+            <p>{name}</p>
+            <input value={name} onChange={handleChange} type="text" />
         </div>
     )
 }
